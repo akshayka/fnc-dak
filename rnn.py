d8c8e982c3593e890671fadbc8ccf4a8430fa730
Kevin Chen <kschen124@gmail.com>
2017-03-03 15:39:27 -0800

    Basic RNN structure taken from Assignment 3, Q2. Initial FNC data reading does basic cleaning and tokenizing. Does not run RNN yet.

---
+++ rnn.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+RNN adapted from Assignment 3 Q2
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import argparse
+import logging
+import sys
+import time
+from datetime import datetime
+
+import tensorflow as tf
+import numpy as np
+import copy
+import pdb
+from util import load_and_preprocess_fnc_data
+from gensim.models import word2vec
+from rnn_cell import RNNCell
+
+class RNNModel():
+    """
+    Implements a recursive neural network with an embedding layer and
+    single hidden layer.
+    """
+
+    def add_placeholders(self):
+        """Generates placeholder variables to represent the input tensors
+
+        These placeholders are used as inputs by the rest of the model building and will be fed
+        data during training.  Note that when "None" is in a placeholder's shape, it's flexible
+        (so we can use different batch sizes without rebuilding the model).
+
+        Adds following nodes to the computational graph
+
+        input_placeholder: Input placeholder tensor of  shape (None, self.max_length, n_features), type tf.int32
+        labels_placeholder: Labels placeholder tensor of shape (None, self.max_length), type tf.int32
+        mask_placeholder:  Mask placeholder tensor of shape (None, self.max_length), type tf.bool
+        dropout_placeholder: Dropout value placeholder (scalar), type tf.float32
+
+        TODO: Add these placeholders to self as the instance variables
+            self.input_placeholder
+            self.labels_placeholder
+            self.mask_placeholder
+            self.dropout_placeholder
+
+        HINTS:
+            - Remember to use self.max_length NOT Config.max_length
+
+        (Don't change the variable names)
+        """
+        ### YOUR CODE HERE (~4-6 lines)
+        self.input_placeholder = tf.placeholder(tf.int32, shape=(None, self.max_length, self.config.n_features))
+        self.labels_placeholder = tf.placeholder(tf.int32, shape=(None, self.max_length))
+        self.mask_placeholder = tf.placeholder(tf.bool, shape=(None, self.max_length))
+        self.dropout_placeholder = tf.placeholder(tf.float32, shape=())
+        ### END YOUR CODE
+
+    def create_feed_dict(self, inputs_batch, mask_batch, labels_batch=None, dropout=1):
+        """Creates the feed_dict for the dependency parser.
+
+        A feed_dict takes the form of:
+
+        feed_dict = {
+                <placeholder>: <tensor of values to be passed for placeholder>,
+                ....
+        }
+
+        Hint: The keys for the feed_dict should be a subset of the placeholder
+                    tensors created in add_placeholders.
+        Hint: When an argument is None, don't add it to the feed_dict.
+
+        Args:
+            inputs_batch: A batch of input data.
+            mask_batch:   A batch of mask data.
+            labels_batch: A batch of label data.
+            dropout: The dropout rate.
+        Returns:
+            feed_dict: The feed dictionary mapping from placeholders to values.
+        """
+        ### YOUR CODE (~6-10 lines)
+        feed_dict = {}
+        if inputs_batch is not None:
+            feed_dict[self.input_placeholder] = inputs_batch
+        if mask_batch is not None:
+            feed_dict[self.mask_placeholder] = mask_batch
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
+        if dropout is not None:
+            feed_dict[self.dropout_placeholder] = dropout
+        ### END YOUR CODE
+        return feed_dict
+
+    def add_embedding(self):
+        """Adds an embedding layer that maps from input tokens (integers) to vectors and then
+        concatenates those vectors:
+
+        TODO:
+            - Create an embedding tensor and initialize it with self.pretrained_embeddings.
+            - Use the input_placeholder to index into the embeddings tensor, resulting in a
+              tensor of shape (None, max_length, n_features, embed_size).
+            - Concatenates the embeddings by reshaping the embeddings tensor to shape
+              (None, max_length, n_features * embed_size).
+
+        HINTS:
+            - You might find tf.nn.embedding_lookup useful.
+            - You can use tf.reshape to concatenate the vectors. See
+              following link to understand what -1 in a shape means.
+              https://www.tensorflow.org/api_docs/python/array_ops/shapes_and_shaping#reshape.
+
+        Returns:
+            embeddings: tf.Tensor of shape (None, max_length, n_features*embed_size)
+        """
+        ### YOUR CODE HERE (~4-6 lines)
+        embedding1 = tf.Variable(self.pretrained_embeddings)
+        embedding2 = tf.nn.embedding_lookup(embedding1, self.input_placeholder)
+        embeddings = tf.reshape(embedding2, [-1, self.max_length, self.config.n_features * self.config.embed_size])
+        ### END YOUR CODE
+        return embeddings
+
+    def add_prediction_op(self):
+        """Adds the unrolled RNN:
+            h_0 = 0
+            for t in 1 to T:
+                o_t, h_t = cell(x_t, h_{t-1})
+                o_drop_t = Dropout(o_t, dropout_rate)
+                y_t = o_drop_t U + b_2
+
+        TODO: There a quite a few things you'll need to do in this function:
+            - Define the variables U, b_2.
+            - Define the vector h as a constant and inititalize it with
+              zeros. See tf.zeros and tf.shape for information on how
+              to initialize this variable to be of the right shape.
+              https://www.tensorflow.org/api_docs/python/constant_op/constant_value_tensors#zeros
+              https://www.tensorflow.org/api_docs/python/array_ops/shapes_and_shaping#shape
+            - In a for loop, begin to unroll the RNN sequence. Collect
+              the predictions in a list.
+            - When unrolling the loop, from the second iteration
+              onwards, you will HAVE to call
+              tf.get_variable_scope().reuse_variables() so that you do
+              not create new variables in the RNN cell.
+              See https://www.tensorflow.org/versions/master/how_tos/variable_scope/
+            - Concatenate and reshape the predictions into a predictions
+              tensor.
+        Hint: You will find the function tf.pack (similar to np.asarray)
+              useful to assemble a list of tensors into a larger tensor.
+              https://www.tensorflow.org/api_docs/python/array_ops/slicing_and_joining#pack
+        Hint: You will find the function tf.transpose and the perms
+              argument useful to shuffle the indices of the tensor.
+              https://www.tensorflow.org/api_docs/python/array_ops/slicing_and_joining#transpose
+
+        Remember:
+            * Use the xavier initilization for matrices.
+            * Note that tf.nn.dropout takes the keep probability (1 - p_drop) as an argument.
+            The keep probability should be set to the value of self.dropout_placeholder
+
+        Returns:
+            pred: tf.Tensor of shape (batch_size, max_length, n_classes)
+        """
+
+        x = self.add_embedding()
+        dropout_rate = self.dropout_placeholder
+
+        preds = [] # Predicted output at each timestep should go here!
+
+        # Use the cell defined below. For Q2, we will just be using the
+        # RNNCell you defined, but for Q3, we will run this code again
+        # with a GRU cell!
+        if self.config.cell == "rnn":
+            cell = RNNCell(Config.n_features * Config.embed_size, Config.hidden_size)
+        elif self.config.cell == "gru":
+            cell = GRUCell(Config.n_features * Config.embed_size, Config.hidden_size)
+        else:
+            raise ValueError("Unsuppported cell type: " + self.config.cell)
+
+        # Define U and b2 as variables.
+        # Initialize state as vector of zeros.
+        ### YOUR CODE HERE (~4-6 lines)
+        xav = tf.contrib.layers.xavier_initializer
+        U = tf.get_variable("U", (self.config.hidden_size, self.config.n_classes), initializer=xav())
+        b2 = tf.get_variable("b2", (self.config.n_classes), initializer=tf.constant_initializer(0))
+        h = tf.zeros((tf.shape(x)[0], self.config.hidden_size), tf.float32)
+        ### END YOUR CODE
+
+        with tf.variable_scope("RNN"):
+            for time_step in range(self.max_length):
+                ### YOUR CODE HERE (~6-10 lines)
+                if time_step > 0:
+                    tf.get_variable_scope().reuse_variables()
+                x_t = x[:,time_step,:]
+                o_t, h= cell(x_t, h)
+                o_drop_t = tf.nn.dropout(o_t, self.dropout_placeholder)
+                y_t = tf.matmul(o_drop_t, U) + b2
+                preds.append(y_t)
+                ### END YOUR CODE
+
+        # Make sure to reshape @preds here.
+        ### YOUR CODE HERE (~2-4 lines)
+        # pdb.set_trace()
+        preds = tf.pack(preds, 1)
+        preds = tf.reshape(preds, [-1, self.max_length, self.config.n_classes])
+        ### END YOUR CODE
+
+        assert preds.get_shape().as_list() == [None, self.max_length, self.config.n_classes], "predictions are not of the right shape. Expected {}, got {}".format([None, self.max_length, self.config.n_classes], preds.get_shape().as_list())
+        return preds
+
+    def add_loss_op(self, preds):
+        """Adds Ops for the loss function to the computational graph.
+
+        TODO: Compute averaged cross entropy loss for the predictions.
+        Importantly, you must ignore the loss for any masked tokens.
+
+        Hint: You might find tf.boolean_mask useful to mask the losses on masked tokens.
+        Hint: You can use tf.nn.sparse_softmax_cross_entropy_with_logits to simplify your
+                    implementation. You might find tf.reduce_mean useful.
+        Args:
+            pred: A tensor of shape (batch_size, max_length, n_classes) containing the output of the neural
+                  network before the softmax layer.
+        Returns:
+            loss: A 0-d tensor (scalar)
+        """
+        ### YOUR CODE HERE (~2-4 lines)
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, self.labels_placeholder)
+        loss = tf.boolean_mask(loss, self.mask_placeholder)
+        loss = tf.reduce_mean(loss)
+        ### END YOUR CODE
+        return loss
+
+    def add_training_op(self, loss):
+        """Sets up the training Ops.
+
+        Creates an optimizer and applies the gradients to all trainable variables.
+        The Op returned by this function is what must be passed to the
+        `sess.run()` call to cause the model to train. See
+
+        https://www.tensorflow.org/versions/r0.7/api_docs/python/train.html#Optimizer
+
+        for more information.
+
+        Use tf.train.AdamOptimizer for this model.
+        Calling optimizer.minimize() will return a train_op object.
+
+        Args:
+            loss: Loss tensor, from cross_entropy_loss.
+        Returns:
+            train_op: The Op for training.
+        """
+        ### YOUR CODE HERE (~1-2 lines)
+        train_op = tf.train.AdamOptimizer(self.config.lr).minimize(loss)        
+        ### END YOUR CODE
+        return train_op
+
+    def preprocess_sequence_data(self, examples):
+        def featurize_windows(data, start, end, window_size = 1):
+            """Uses the input sequences in @data to construct new windowed data points.
+            """
+            ret = []
+            for sentence, labels in data:
+                from util import window_iterator
+                sentence_ = []
+                for window in window_iterator(sentence, window_size, beg=start, end=end):
+                    sentence_.append(sum(window, []))
+                ret.append((sentence_, labels))
+            return ret
+
+        examples = featurize_windows(examples, self.helper.START, self.helper.END)
+        return pad_sequences(examples, self.max_length)
+
+    def consolidate_predictions(self, examples_raw, examples, preds):
+        """Batch the predictions into groups of sentence length.
+        """
+        assert len(examples_raw) == len(examples)
+        assert len(examples_raw) == len(preds)
+
+        ret = []
+        for i, (sentence, labels) in enumerate(examples_raw):
+            _, _, mask = examples[i]
+            labels_ = [l for l, m in zip(preds[i], mask) if m] # only select elements of mask.
+            assert len(labels_) == len(labels)
+            ret.append([sentence, labels, labels_])
+        return ret
+
+    def predict_on_batch(self, sess, inputs_batch, mask_batch):
+        feed = self.create_feed_dict(inputs_batch=inputs_batch, mask_batch=mask_batch)
+        predictions = sess.run(tf.argmax(self.pred, axis=2), feed_dict=feed)
+        return predictions
+
+    def train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch):
+        feed = self.create_feed_dict(inputs_batch, labels_batch=labels_batch, mask_batch=mask_batch,
+                                     dropout=Config.dropout)
+        _, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
+        return loss
+
+    def __init__(self, helper, config, pretrained_embeddings, report=None):
+        super(RNNModel, self).__init__(helper, config, report)
+        self.max_length = min(Config.max_length, helper.max_length)
+        Config.max_length = self.max_length # Just in case people make a mistake.
+        self.pretrained_embeddings = pretrained_embeddings
+
+        # Defining placeholders.
+        self.input_placeholder = None
+        self.labels_placeholder = None
+        self.mask_placeholder = None
+        self.dropout_placeholder = None
+
+        self.build()
+
+def do_train(args):
+    # Set up some parameters.
+    bodies, stances = load_and_preprocess_fnc_data(args)
+    # read
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Trains and tests RNN model')
+    subparsers = parser.add_subparsers()
+
+    command_parser = subparsers.add_parser('train', help='')
+    command_parser.add_argument('-dtb', '--train-bodies', type=argparse.FileType('r'), default="fnc-1-data/train_bodies.csv", help="Training data")
+    command_parser.add_argument('-dts', '--train-stances', type=argparse.FileType('r'), default="fnc-1-data/train_stances.csv", help="Training data")
+    command_parser.add_argument('-v', '--vocab', type=argparse.FileType('r'), default="supp_data/vocab.txt", help="Path to vocabulary file")
+    command_parser.add_argument('-vv', '--vectors', type=argparse.FileType('r'), default="supp_data/wordVectors.txt", help="Path to word vectors file")
+    command_parser.set_defaults(func=do_train)
+
+    ARGS = parser.parse_args()
+    if ARGS.func is None:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        ARGS.func(ARGS)
---
+++ util.py
@@ -0,0 +1,57 @@
+import os
+import re
+from collections import Counter
+import pdb
+
+
+import numpy as np
+
+
+def read_stances(fstream):
+	ret = [[], [], []]
+	fstream.readline()
+	for line in fstream:
+		first = line.rfind(',')
+		second = line[:first].rfind(',')
+		header = line[:second]
+		if header[0] == '\"' and header[-1] == '\"': # clean out extra quotes for headers with commas
+			header = header[1:-1]
+		cleaned = re.findall(r"\w+|[^\w\s]", header) # split all punctuation, paranthesis, quotes, etc. from words
+		ret[0].append(cleaned)
+		ret[1].append(int(line[second+1:first]))
+		ret[2].append(line[first+1:])
+	return ret 		
+
+
+
+def read_bodies(fstream):
+	""" 
+	Basic pre-processing for bodies. Bodies can span multiple lines, beginning and ends denoted by quotes.
+	Quotes within a body are denoted with double quotes (""), 
+	Each body ends with "\n, so we take off the last two characters when reading them in.
+	"""
+	ret = {}
+	fstream.readline()
+	body_id = None
+	body = []
+	for line in fstream:
+		if body_id is None:
+			first = line.find(',')
+			body_id = int(line[:first])
+			line = line[first +2:]
+		if len(line) > 5 and line[-2] == '\"' and (line[-3] != '\"' or line[-4:-1] == '\"\"\"'): # check if line is last in body
+			cleaned = line[:-2].replace('\"\"', '\"')
+			body += re.findall(r"\w+|[^\w\s]", cleaned)
+			ret[body_id] = body
+			body_id = None
+			body = []
+		else:
+			body += re.findall(r"\w+|[^\w\s]", line.replace('\"\"', '\"'))
+	return ret
+
+
+
+def load_and_preprocess_fnc_data(args):
+	bodies = read_bodies(args.train_bodies)
+	stances = read_stances(args.train_stances)
+	return bodies, stances
